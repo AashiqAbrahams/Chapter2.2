2b23e4a53cf0277b267cd110b4a5e3d20c4f26d2
Aashiq Abrahams <aashiqabrahamszb@gmail.com>
2019-03-03 21:04:53 +0200

    added animals

---
+++ src/main/java/com/walmers/Animal.java
@@ -0,0 +1,22 @@
+package com.walmers;
+
+import com.walmers.Interfaces.Move;
+import com.walmers.Interfaces.Sound;
+
+public abstract class Animal implements Sound, Move {
+
+    private String type;
+
+    public Animal(String type){
+        this.type = type;
+    }
+
+    public String getAnimalType() {
+        return type;
+    }
+
+    public void setAnimalType(String type) {
+        this.type = type;
+    }
+
+}
---
+++ src/main/java/com/walmers/Horse.java
@@ -0,0 +1,14 @@
+package com.walmers;
+
+public class Horse extends Animal {
+
+    public Horse() {
+        super("Horse");
+    }
+    public String sound(){
+        return "neighs";
+    }
+    public String move() {
+        return "gallops";
+    }
+}
