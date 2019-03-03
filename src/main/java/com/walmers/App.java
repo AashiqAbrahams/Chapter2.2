8907c5a51df9b5f7793d426a25aea66876f4f9a3
Aashiq Abrahams <aashiqabrahamszb@gmail.com>
2019-03-03 21:50:04 +0200

    edited App class

--- src/main/java/com/walmers/App.java
+++ src/main/java/com/walmers/App.java
@@ -1,15 +1,76 @@
 package com.walmers;
 
-/**
- * Hello world!
- *
- */
+import com.walmers.Interfaces.Move;
+import com.walmers.Interfaces.Sound;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
 public class App 
 {
     public static void main( String[] args )
     {
 
+        try{
 
+//            final String [] animals = {"Wolf","Eagle","Horse"};
+//
+//                List<String> list = new ArrayList<>();
+//
+//                for(String name : animals)
+//                    list.add(name);
+//
+//                System.out.println("Please enter the names of one of the following animals: " );
+//
+//                for(int i = 0; i < list.size();i++)
+//                    System.out.printf("%s, ", list.get(i));
+
+
+                    System.out.println("Enter Wolf, Horse or Eagle to see their move and sound action...");
+                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+                    String input;
+
+                    while((input = br.readLine())!=null)
+                        if (input.toLowerCase().equals("wolf")) {
+
+                            Animal animal = new Wolf();
+                            doSpeak(animal);
+                            doMove(animal);
+
+
+                        } else if (input.toLowerCase().equals("eagle")) {
+
+                            Animal  animal = new Eagle();
+                            doSpeak(animal);
+                            doMove(animal);
+
+                        } else if (input.toLowerCase().equals("horse")) {
+
+                            Animal  animal = new Horse();
+                            doSpeak(animal);
+                            doMove(animal);
+
+                }
+                }catch(IOException io){
+                    io.printStackTrace();
+
+            }
+            //removing dependency
+
+            private static void doMove(Move mover){
+
+                System.out.println(mover.move());
+            }
+
+            private static void doSpeak(Sound speak){
+
+                System.out.println(speak.sound());
+            }
+        }
 
     }
-}
+
